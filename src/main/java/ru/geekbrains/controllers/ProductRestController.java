5f12e8937689121d17d0f933e5eb390aee045ccc
UgryumovAlex <ugryumov_alex@mail.ru>
2021-09-18 12:43:34 +0300

    Initial commit

---
+++ src/main/java/ru/geekbrains/controllers/ProductRestController.java
@@ -0,0 +1,7 @@
+package ru.geekbrains.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductRestController {
+}
---
+++ src/main/java/ru/geekbrains/services/ProductService.java
@@ -0,0 +1,7 @@
+package ru.geekbrains.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+}
